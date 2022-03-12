818ecb1d0fa085f5a69f88d660f6f5ef4c1a09ea
CarlosEhduardo <carlos0eduardo074@gmail.com>
2022-03-12 14:44:33 -0300

    Template do proxy

--- backend/src/main/java/com/dio_class/devweek/controllers/FaixaEtariaController.java
+++ backend/src/main/java/com/dio_class/devweek/controllers/FaixaEtariaController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api")
 public class FaixaEtariaController {
     private final FaixaEtariaRepository repository;
 
